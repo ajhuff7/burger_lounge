b2bd0ee0abf0cf3f42a301361d2af8859bef4978
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-10-21 17:07:34 -0700

    Init file set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bluecheese Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", TRUE);
